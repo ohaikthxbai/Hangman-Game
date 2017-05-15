033c5fbe7704d4196f9e71e8bafbeaed2aca570f
Justin G <MBA13-JG@Justins-MacBook-Air.local>
2017-05-15 13:18:07 -0500

    changed theme to smash bros, added math.random for rng words

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,24 +10,41 @@
 	number of losses and wins are recorded and displayed
 	when the game is over, a new game starts with another
 	randomly generated word from the list
+	if the player loses, alert the player has lost, try again
+	if player wins, alert with uplifting message, play again
+
 
 */
 
-// word list
-var wordLibrary = [
-	"finn",
-	"princess bubblegum",
-	"jake",
-	"lumpy space princess",
-	"ice king",
-	"lemongrab",
-	"gunther",
-	"peppermint butler",
-	"flame princess",
-	"marceline",
-	"beemo",
-	"tree trunks",
-	"cinnamon bun",
-]
+// word array
+var pickCharacter = [
+	"luigi",
+	"mario",
+	"dk",
+	"link",
+	"samus",
+	"falcon",
+	"ness",
+	"yoshi",
+	"kirby",
+	"fox",
+	"pikachu",
+	"jigglypuff",
+];
 
 // variables
+
+var wins = 0;
+var losses = 0;
+var incorrectGuess = [];
+var guessAmount = 10;
+
+var toPlay = function() {
+	// computer will randomly choose word from pickCharacter
+	playWord = pickCharacter[Math.floor(Math.random() * pickCharacter.length)];
+	console.log(playWord);
+};
+
+
+// tested; function works so far.
+toPlay();
