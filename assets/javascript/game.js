ed2acd17737f74c4259d0d1f428260dc57d1eeb9
Justin G <MBA13-JG@Justins-MacBook-Air.local>
2017-05-19 23:26:50 -0500

    game worksgit add .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,71 +26,129 @@
 */
 
 // variables
-var characters = 
-[
-	"luigi",
-	"mario",
-	"dk",
-	"link",
-	"samus",
-	"falcon",
-	"ness",
-	"yoshi",
-	"kirby",
-	"fox",
-	"pikachu",
-	"jigglypuff",
+var characters = [
+    "luigi",
+    "mario",
+    "dk",
+    "link",
+    "samus",
+    "falcon",
+    "ness",
+    "yoshi",
+    "kirby",
+    "fox",
+    "pikachu",
+    "jigglypuff",
 ];
 
+var playWord = "";
+var splitWord = [];
+var spaceWord = 0;
 var wins = 0;
 var losses = 0;
-var incorrectGuess = [];
+var wrongGuess = [];
+var userKeys = [];
 var keystroke = "";
 var spaces = [];
-var guessAmount = 11;
+var guessAmt = 10;
 
 // setting up the game
-var toPlay = function() 
-{
-	// computer will randomly choose word from characters
-	// math.floor takes any number
-	playWord = characters[Math.floor(Math.random() * characters.length)];
-	// break secret word into an array of characters
-	splitWord = playWord.split('');
-	// count the number of spaces after each character
-	wordSpaces = splitWord.length;
-	//replace secret word with underscores
-	for (i = 0; i < wordSpaces; i++) {
-		spaces.push("_");
-	}
+var readyGO = function() {
+    //ready game
+    spaces = [];
+    wrongGuess = [];
+    // computer will randomly choose word from characters
+    playWord = characters[Math.floor(Math.random() * characters.length)];
+    // break secret word into an array of characters
+    splitWord = playWord.split('');
+    // count the number of spaces after each character
+    spaceWord = splitWord.length;
+    //replace secret word with underscores
+    for (i = 0; i < spaceWord; i++) {
+        spaces.push("_");
+    }
 
-	//might be located in a different function
-	//check if keystroke equals a letter from splitWord
+    // initial setup of the hangman game
+    document.getElementById('win-count').innerHTML = wins;
+    document.getElementById('lose-count').innerHTML = losses;
+    document.getElementById('num-guess').innerHTML = guessAmt;
+    document.getElementById('wrong-guess').innerHTML = wrongGuess;
+    document.getElementById('empty-space').innerHTML = spaces.join(" ");
 
+    //TESTING 
+    console.log(splitWord);
+    console.log(spaceWord);
+    console.log(wins);
+    console.log(losses);
+    console.log(guessAmt);
 
-
-	//TESTING 
-	console.log(splitWord);
-	//console.log(wordSpaces);
-	console.log(wins);
-	console.log(losses);
-	console.log(guessAmount);
-	//console.log(incorrectGuess);
-	document.getElementById('win-count').innerHTML = wins;
-	document.getElementById('lose-count').innerHTML = losses;
-	document.getElementById('num-guess').innerHTML = guessAmount;
-	document.getElementById('wrong-guess').innerHTML = incorrectGuess;
-	document.getElementById('empty-space').innerHTML = spaces.join(" ");
 };
+
+// checks the user's input to the letters of the word that's being played
+var checkInput = function(letter) {
+	// currently no correct letter
+	var rightLetter = false;
+
+	for (i = 0; i < spaceWord; i++) {
+		// if the current letter in the word matches the user input,
+		// mark rightLetter as true
+		if (playWord[i] === letter) {
+			rightLetter = true;
+		}
+	}
+	// if the user's input is correct, replace the underscore with the correct letter
+	if (rightLetter) {
+		for (i = 0; i < spaceWord; i++) {
+			if(playWord[i] === letter) {
+				spaces[i] = letter;
+				document.getElementById('empty-space').innerHTML = spaces.join(" ");
+			}
+		}
+	}
+	// otherwise reduce the amount of guesses by one and display the incorrect guess. 
+	else {
+		guessAmt--;
+		document.getElementById('num-guess').innerHTML = guessAmt;
+		wrongGuess.push(letter);
+		document.getElementById('wrong-guess').innerHTML = wrongGuess;
+	}
+}
+
+// results of the player's win or loss
+var gameSet = function() {
+	if (splitWord.join(" ") === spaces.join(" ")) {
+		wins++;
+		document.getElementById('win-count').innerHTML = wins;
+		alert("GAME SET!! Congratulations! You win!");
+		readyGO();
+	}
+	else if (guessAmt === 0) {
+		document.getElementById('num-guess').innerHTML = guessAmt;
+		alert("GAME OVER... Continue?")
+		readyGO();
+	}
+}
 
 
 // tested; function works so far.
-toPlay();
+readyGO();
 
 // when the key is pressed by the player
 document.onkeyup = function(event) {
-	var keystroke = event.key;
-	console.log(keystroke);
+    keystroke = event.key;
+    if (userKeys.indexOf(keystroke) >= 0) {
+    	//meaning it already exists in the array
+    }
+    else {
+    	//the keystroke is not in the array
+    	//userKeys.push(keystroke);
+    	checkInput(keystroke);
+    	gameSet();
+    	//test
+    	//console.log(userKeys);
+    	console.log(wrongGuess);
+    }
+    
 
 
 };
